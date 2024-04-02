03177d991d65a2fa6e7f70655fa737ae95f774e0
Natthan Guilhot <72070680+natthanguilhot@users.noreply.github.com>
2024-04-02 15:18:29 +0200

    npm init + tailwind

--- popup/options.js
+++ popup/options.js
@@ -1,10 +1,31 @@
-document.querySelector("button#savespeedvideo").addEventListener("click", () => {
+document.querySelector("button#saveAndRefreshSpeedVideo").addEventListener("click", () => {
+	const isActive = document.querySelector("input#activespeedvideo").checked;
+	const speed = document.querySelector("input#speedspeedvideo").value > 16 ? 16 : document.querySelector("input#speedspeedvideo").value;
+	chrome.storage.sync
+		.set({ isActive, speed })
+		.then(() => console.log("Options sauvegardées."))
+		.catch((err) => console.log("ERROR : ", err));
+	chrome.tabs.query({ active: true, currentWindow: true }, function (arrayOfTabs) {
+		chrome.tabs.reload(arrayOfTabs[0].id);
+	});
+	window.close();
+});
+
+document.querySelector("button#saveSpeedVideo").addEventListener("click", () => {
 	const isActive = document.querySelector("input#activespeedvideo").checked;
 	const speed = document.querySelector("input#speedspeedvideo").value;
 	chrome.storage.sync
 		.set({ isActive, speed })
 		.then(() => console.log("Options sauvegardées."))
 		.catch((err) => console.log("ERROR : ", err));
+	window.close();
+});
+
+document.querySelector("button#refreshPage").addEventListener("click", () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (arrayOfTabs) {
+		chrome.tabs.reload(arrayOfTabs[0].id);
+	});
+	window.close();
 });
 
 async function init() {

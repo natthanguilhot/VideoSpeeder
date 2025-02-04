aeef4053a9471537d5ef842d20bb91e2639f2f6a
Natthan Guilhot <72070680+natthanguilhot@users.noreply.github.com>
2025-02-04 13:29:51 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,15 +1,14 @@
 async function start() {
 	const { isActive, speed } = await chrome.storage.sync.get(["isActive", "speed"]);
-	setInterval(() => {
-		if (isActive) {
-			const videos = document.querySelectorAll("video");
-			videos.forEach(function (video) {
-				if (video.playbackRate != speed) {
-					video.playbackRate = speed;
-					console.log("video.playbackRate = " + video.playbackRate);
-				}
-			});
-		}
-	}, 2000);
+
+	if (isActive) {
+		const videos = document.querySelectorAll("video");
+		videos.forEach(function (video) {
+			if (video.playbackRate != speed) {
+				video.playbackRate = speed;
+				console.log("video.playbackRate = " + video.playbackRate);
+			}
+		});
+	}
 }
 start();
